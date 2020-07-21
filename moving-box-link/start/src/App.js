9f4b6f1ded924bcb40860f8a86ee5b6bd3afa2fe
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-07-21 11:39:21 +0100

    link move finished

--- moving-box-link/start/src/App.js
+++ moving-box-link/start/src/App.js
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import useMovement from './useMovement';
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="app">
-      <canvas />
+  const canvasRef = useRef(null);
+  const linkDownRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const linkUpRef = useRef(null);
+  const linkLeftRef = useRef(null);
+  const { x, y, move, direction } = useMovement();
 
-      <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
+  }, []);
+
+  useEffect(() => {
+    const context = canvasRef.current.getContext('2d');
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    switch (direction) {
+      case 'down':
+        context.drawImage(linkDownRef.current, x, y);
+        break;
+      case 'left':
+        context.drawImage(linkLeftRef.current, x, y);
+        break;
+      case 'up':
+        context.drawImage(linkUpRef.current, x, y);
+        break;
+      case 'right':
+        context.drawImage(linkRightRef.current, x, y);
+        break;
+      default:
+        break;
+    }
+  }, [x, y, direction]);
+
+  return (
+    <div className='app'>
+      <canvas ref={canvasRef} />
+
+      <div className='arrows'>
+        <button onClick={() => move('up')}>Up</button>
+        <button onClick={() => move('left')}>Left</button>
+        <button onClick={() => move('down')}>Down</button>
+        <button onClick={() => move('right')}>Right</button>
       </div>
 
-      <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
+      <div className='images'>
+        <img
+          ref={linkDownRef}
+          src='https://i.imgur.com/JYUB0m3.png'
+          alt='Down'
+        />
+        <img
+          ref={linkRightRef}
+          src='https://i.imgur.com/GEXD7bk.gif'
+          alt='Right'
+        />
+        <img ref={linkUpRef} src='https://i.imgur.com/XSA2Oom.gif' alt='Up' />
+        <img
+          ref={linkLeftRef}
+          src='https://i.imgur.com/4LGAZ8t.gif'
+          alt='Left'
+        />
       </div>
     </div>
   );
