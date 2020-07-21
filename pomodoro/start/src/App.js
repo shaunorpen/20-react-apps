dd13d818e4c7ae3da708064dd7803de2986dafb3
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-07-21 13:21:29 +0100

    add messages

--- pomodoro/start/src/App.js
+++ pomodoro/start/src/App.js
@@ -5,11 +5,20 @@ function padTime(time) {
   return time.toString().padStart(2, '0');
 }
 
+const messages = [
+  `Come on! You only just started!`,
+  `I knew you'd give up...slacker!`,
+  `You can do this...just keep going!`,
+  `Yeah, probably about time to quit`,
+  `Maybe try again tomorrow?`,
+];
+
 export default function App() {
   const [title, setTitle] = useState('Let the countdown begin!');
   const [timeLeft, setTimeLeft] = useState(25 * 60);
   const [isRunning, setIsRunning] = useState(false);
   const [showReset, setShowReset] = useState(false);
+  const [messageID, setMessageID] = useState(0);
   const intervalRef = useRef(null);
 
   function startTimer() {
@@ -33,12 +42,13 @@ export default function App() {
   function stopTimer() {
     if (intervalRef.current === null) return;
 
-    setTitle('Keep it up!');
+    setTitle(messages[messageID]);
 
     clearInterval(intervalRef.current);
     intervalRef.current = null;
 
     setIsRunning(false);
+    setMessageID((messageID) => (messageID + 1) % messages.length);
   }
 
   function resetTimer() {
