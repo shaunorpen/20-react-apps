98017befe699c773c25a2b1cc2b2e0c15a7ce54c
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-07-21 15:54:59 +0100

    encode images as base64

--- moving-box-link/start/src/App.js
+++ moving-box-link/start/src/App.js
@@ -1,5 +1,4 @@
-import React, { useRef, useEffect } from 'react';
-import useMovement from './useMovement';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 
 export default function App() {
@@ -8,12 +7,59 @@ export default function App() {
   const linkRightRef = useRef(null);
   const linkUpRef = useRef(null);
   const linkLeftRef = useRef(null);
-  const { x, y, move, direction } = useMovement();
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [direction, setDirection] = useState('down');
+
+  function move(dir) {
+    setDirection(dir);
+    switch (dir) {
+      case 'up':
+        setY((y) => (y >= 20 ? y - 20 : y));
+        break;
+      case 'left':
+        setX((x) => (x >= 20 ? x - 20 : x));
+        break;
+      case 'down':
+        setY((y) => (y <= window.innerHeight - 30 ? y + 20 : y));
+        break;
+      case 'right':
+        setX((x) => (x <= window.innerWidth - 30 ? x + 20 : x));
+        break;
+      default:
+        break;
+    }
+  }
 
   useEffect(() => {
     const context = canvasRef.current.getContext('2d');
     context.canvas.height = window.innerHeight;
     context.canvas.width = window.innerWidth;
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(e) {
+      switch (e.key) {
+        case 'ArrowUp':
+          move('up');
+          break;
+        case 'ArrowLeft':
+          move('left');
+          break;
+        case 'ArrowDown':
+          move('down');
+          break;
+        case 'ArrowRight':
+          move('right');
+          break;
+        default:
+          break;
+      }
+    }
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
   }, []);
 
   useEffect(() => {
@@ -51,18 +97,22 @@ export default function App() {
       <div className='images'>
         <img
           ref={linkDownRef}
-          src='https://i.imgur.com/JYUB0m3.png'
+          src={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAgAgMAAAB4LiDsAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAADFBMVEUAiP+4+Bj/oETkXBC3SAr7AAAAAXRSTlMAQObYZgAAAAFiS0dEAxEMTPIAAABqSURBVBjTVc+hFsAgCIVhimXFV1uhWHw1C4XCq1kslju24OBP3zkQgIgK801vB+wljA6sKwFegu4mphnbMi71LKI029IDKqDKmD9gWMwWsBW2mgb4SHx/JNjiRgGAfO8c+BWzIsKHkyjgAWfBggG544yMAAAAAElFTkSuQmCC`}
           alt='Down'
         />
         <img
           ref={linkRightRef}
-          src='https://i.imgur.com/GEXD7bk.gif'
+          src={`data:image/gif;base64,R0lGODlhHgAgAPEAAORcEACI/7j4GP+gRCH/C05FVFNDQVBFMi4wAwEAAAAh+QQBGQABACwAAAAAHgAgAAACqYyPqcvtj4ScEjpKbcIY+O9BHAWCDDcMZMkqqDqx7TEKaSqX7ngPubkRGCg9HKAnSe2Gk6LveEsOLgJQz3OF9nbWG9arvTFYTqchtCCXtwH0iVc+A6gvc3seBBGLXTet+rGX9eUnl+MkQYiH8BOWkgjlB3kYFYMnCUj5aCkXYcnC4Yc5WRK6eGcIUEOqJ4R657GaWeqKBirb8cFIi/t5mlqYMKMwnKaTUAAAOw==`}
           alt='Right'
         />
-        <img ref={linkUpRef} src='https://i.imgur.com/XSA2Oom.gif' alt='Up' />
+        <img
+          ref={linkUpRef}
+          src={`data:image/gif;base64,R0lGODlhGAAgAPEAALj4GORcEACI//+gRCH/C05FVFNDQVBFMi4wAwEAAAAh+QQBGQACACwAAAAAGAAgAAACgpQPqZvnzyJ4UFpq7D1jZB0ZXfeBizia2jioFhsErhLD8gzU7BDHVh/YjYC/HgsF9NGSJA4vuUgGPQelVJqIOaxXYi6w7c2ABq8K2DGb0E/l2di+gciCtHjdQ3Wy3XujnvLVJzeBgHNiePgXNpdE4SYhhSFAV3Y1WUl5OcmZyYmRWQAAOw==`}
+          alt='Up'
+        />
         <img
           ref={linkLeftRef}
-          src='https://i.imgur.com/4LGAZ8t.gif'
+          src={`data:image/gif;base64,R0lGODlhHgAgAPEAAORcEACI/7j4GP+gRCH/C05FVFNDQVBFMi4wAwEAAAAh+QQBGQABACwAAAAAHgAgAAACpoyPqcvtr4ScEjpK7QG8c4xBngdmzYgC1DCASjqubbjAHsuWwmtz+OxK5AQ/3w+4C1SEsyLgiDQsoUZWdUBKIqhP3DXLM0C5HgZHPHaWawA0VOc4B9LwU/d2pIATRvxP37GE0KdCdNTDY1VIlhIW8DGhliL42FYpowhDKWcACYhi0mkpCqI5MWgpYaqzJqc6qSOwJuoZ+9mBWmv7OrsxEvfL1suzVgAAOw==`}
           alt='Left'
         />
       </div>
