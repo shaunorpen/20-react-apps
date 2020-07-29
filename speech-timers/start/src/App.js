1e8fa226aeaa0ea95df8b07f79e8022286bfe865
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-07-29 19:29:33 +0100

    wip

--- speech-timers/start/src/App.js
+++ speech-timers/start/src/App.js
@@ -3,50 +3,48 @@ import { useStopwatch } from 'react-timer-hook';
 import { useSpeechSynthesis } from 'react-speech-kit';
 import './App.css';
 
-import TimerSlot from './components/TimerSlot';
-
 export default function App() {
   const [timers, setTimers] = useState([
     { time: 2, text: 'this is my message' },
     { time: 5, text: 'hello' },
     { time: 8, text: 'whats up' },
   ]);
-
   const { seconds, isRunning, start, reset } = useStopwatch();
-  const { speak, supported, speaking } = useSpeechSynthesis();
+  const { speak, speaking, supported } = useSpeechSynthesis();
 
   const doReset = useCallback(() => reset(), []);
-  const doSpeak = useCallback((...p) => {
-    return speak(...p);
-  }, []);
+  const doSpeak = useCallback((...p) => speak(...p), []);
 
   useEffect(() => {
     const foundTimer = timers.find((t) => t.time === seconds);
     if (foundTimer) doSpeak({ text: foundTimer.text });
+
+    // check to see if seconds is greater than the last timers time
     if (seconds > timers[timers.length - 1].time) doReset();
-  }, [seconds, timers, speak, doReset]);
+  }, [seconds, timers, doSpeak, doReset]);
 
   function updateTimers(index, time, text) {
     const newTimers = [...timers];
     newTimers[index].time = time;
     newTimers[index].text = text;
+
     setTimers(newTimers);
   }
 
   function addTimer() {
-    const newTimers = [...timers, { time: 100, text: 'Yo!' }];
+    const newTimers = [...timers, { time: 100, text: 'yooo' }];
     setTimers(newTimers);
   }
 
   if (!supported) {
-    return <div>Your browser is not supported. Sorry!</div>;
+    return <div>Your browser is not supported. Sorry.</div>;
   }
 
   return (
-    <div className='app'>
+    <div className="app">
       <h2>Talk the Talk</h2>
 
-      <div className='timers'>
+      <div className="timers">
         {/* timers go here */}
         {timers.map((timer, index) => (
           <TimerSlot
@@ -56,7 +54,8 @@ export default function App() {
             updateTimers={updateTimers}
           />
         ))}
-        <button className='add-button' onClick={addTimer}>
+
+        <button className="add-button" onClick={addTimer}>
           Add
         </button>
       </div>
@@ -65,19 +64,47 @@ export default function App() {
       <h2>{seconds}</h2>
 
       {/* buttons */}
-      <div className='buttons'>
+      <div className="buttons">
         {!isRunning && (
-          <button className='start-button' onClick={start}>
+          <button className="start-button" onClick={start}>
             Start
           </button>
         )}
+
         {isRunning && (
-          <button className='stop-button' onClick={reset}>
+          <button className="stop-button" onClick={reset}>
             Stop
           </button>
         )}
-        {speaking && <div>I am speaking...</div>}
+
+        {speaking && <p>I am speaking...</p>}
       </div>
     </div>
+  );
+}
+
+function TimerSlot({ index, timer, updateTimers }) {
+  const [time, setTime] = useState(timer.time);
+  const [text, setText] = useState(timer.text);
+
+  function handleBlur() {
+    updateTimers(index, time, text);
+  }
+
+  return (
+    <form className="timer" key={index}>
+      <input
+        type="number"
+        value={time}
+        onChange={(e) => setTime(+e.target.value)}
+        onBlur={handleBlur}
+      />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onBlur={handleBlur}
+      />
+    </form>
   );
 }
